b3fe87d7a4b2e9fae94e1d58b471f09bef319f36
aniketk2112 <aniketkanurkar@icloud.com>
2024-02-14 13:23:37 -0500

    Login Page design completed

---
+++ frontend/src/components/Login/login.js
@@ -0,0 +1,39 @@
+// Login.js
+
+import React, { useState } from 'react';
+import './login.css'; // Import your stylesheet
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Handle login logic here
+        console.log('Logging in with:', username, password);
+    };
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form>
+                <label>Username:</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter your username"
+                />
+                <label>Password:</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter your password"
+                />
+                <button type="button" onClick={handleLogin}>Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
